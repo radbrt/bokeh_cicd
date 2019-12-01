f8d6f9a74f39f8764b31424728c83517e02ae2c9
radbrt <henning.holgersen@gmail.com>
2019-12-01 19:47:55 +0100

    deadend on multi_line

--- utdstat.py
+++ utdstat.py
@@ -6,27 +6,42 @@ from bokeh.layouts import row, column
 from bokeh.palettes import viridis
 import pandas as pd
 
+
+def create_col_column(colors, column):
+    u_vals = sorted(set(column))
+    d = {k: v for k, v in zip(u_vals, colors)}
+    colcols = [d[val] for val in column]
+    return(colcols)
+
+
 df = pd.read_csv('df.csv', sep=";")
 df['aar'] = df['intervall_ar'].str[0:4].astype(int)
 
 TOOLTIPS=[
     ("Antall", "@elever"),
-    ("Year", "@aar"),
-    ("$", "@andel_elever_prosent")
+    ("År", "@aar"),
+    ("Prosent", "@andel_elever_prosent"),
+    ("Foreldrenes utdanning", "@forutd"),
+    ("Farge", "$color")
 ]
 
-palette = ["#"]
+#viridis_dict = {k: v for k, v in zip(sorted(set(df['foreldrenes_utdanningsniva'])), viridis(6))}
+df['foreldrefarge'] = create_col_column(viridis(6), df['foreldrenes_utdanningsniva'])
 
 def create_plot():
-    p = figure(plot_height=400, plot_width=800)
+    p = figure(plot_height=400, plot_width=800, tooltips=TOOLTIPS, toolbar_location=None)
     p.xaxis.axis_label = "Skoleår"
     p.yaxis.axis_label = "Andel"
     
-    df2 = df.loc[ (df['kjonn']==kjonnselect.value) & (df['fullforingsgrad'] == fullfselect.value) & (df['studieretning_utdanningsprogram']==studretnselect.value)]
-    dfl = df2.groupby('foreldrenes_utdanningsniva')['elever'].apply(list)
-    dflaar = df2.groupby('foreldrenes_utdanningsniva')['aar'].apply(list)
+    # Apply list on grouped dataframe returns list of lists - must be run on each variable.
+    df2 = df.loc[ (df['kjonn']==kjonnselect.value) & (df['fullforingsgrad'] == fullfselect.value) & (df['studieretning_utdanningsprogram']==studretnselect.value)].groupby('foreldrenes_utdanningsniva')
+    dfl = df2['elever'].apply(list)
+    dflaar = df2['aar'].apply(list)
+    dflandel = df2['andel_elever_prosent'].apply(list)
+    dfl_forutd = df2['foreldrenes_utdanningsniva'].apply(list)
+    dflfarge = df2['foreldrefarge'].apply(list)
 
-    #source = ColumnDataSource(data={'aar': dflaar, 'elever': dfl, 'palette': viridis(6) })
+    source = ColumnDataSource(data={'aar': dflaar, 'elever': dfl, 'palette': dflfarge, 'forutd': dfl_forutd, 'andel_elever_prosent':  dflandel})
     p.multi_line(xs=dflaar, ys=dfl, color=viridis(6) )
     return(p)
 
